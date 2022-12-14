8117989c647a67a3fb7db8b1f21ad92be6f64252
Long Vu <78052960+Bizarespace@users.noreply.github.com>
2022-12-14 13:20:03 -0800

    Add files via upload

---
+++ countToTen.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 14 13:16:46 2022
+@author: LongVu
+Simple program to count to 10
+"""
+def countToTen():
+    iterator = 1
+    while (iterator < 11):
+        print(iterator)
+        iterator += 1
+
+countToTen()
